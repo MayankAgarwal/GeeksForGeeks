d89093f739cf5c953fb81d1c5c3e6dde5e90fb0c
Mayank Agarwal <mayank.ag.agarwal@oracle.com>
2015-03-27 00:27:12 +0530

    [Correct]: Check if path is circular or not. Write from correct repository.

--- check-if-a-given-sequence-of-moves-for-a-robot-is-circular-or-not.py
+++ check-if-a-given-sequence-of-moves-for-a-robot-is-circular-or-not.py
@@ -1,3 +1,20 @@
+'''
+URL: http://www.geeksforgeeks.org/check-if-a-given-sequence-of-moves-for-a-robot-is-circular-or-not/
+====
+
+Python 2.7 compatible
+
+Problem statement:
+====================
+
+Given a sequence of moves for a robot, check if the sequence is circular or not. A sequence of moves is circular if first and last positions of robot are same. A move can be on of the following.
+
+  G - Go one unit
+  L - Turn left
+  R - Turn right 
+
+'''
+
 from operator import add
 import math
 
@@ -20,13 +37,17 @@ for move in moves:
 
 		angle = heading[1]
 		step = heading[0]
+
+		# move_coord holds the x and y coordinate movement for the robot
 		move_coord = [ round(step*math.cos(math.radians(angle))), round(step*math.sin(math.radians(angle))) ]
 		current_position = map(add, current_position, move_coord)
 	
 	elif move.upper() == "L":
+		# turn the robot 90 degrees anti-clockwise
 		heading = map(add, heading, [0, 90])
 
 	elif move.upper() == "R":
+		# turn the robot 90 degrees clockwise
 		heading = map(add, heading, [0, -90])
 
 
